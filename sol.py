9199a02cac9e5273b361f5cdee7b9c999e58517e
Saul Field <saul.field@gmail.com>
2019-01-22 01:21:16 -0500

    Declarations and basic statements

--- sol.py
+++ sol.py
@@ -2,7 +2,29 @@ import sys
 from enum import Enum
 from parsley import makeGrammar
 
-Type = Enum('Type', 'Num Bool')
+Type = Enum('Type', 'Null Num Bool')
+StmtKind = Enum('StmtKind', 'Print')
+
+op_table = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y
+}
+
+global_env = {}
+
+class Decl:
+    def __init__(self, name, exp):
+        self.name = name
+        self.exp = exp
+        if exp is None:
+            self.exp = Literal(Type.Null, None)
+
+class Stmt:
+    def __init__(self, kind, exp):
+        self.kind = kind
+        self.exp = exp
 
 class Literal:
     def __init__(self, type, val):
@@ -10,6 +32,7 @@ class Literal:
         self.val = val
 
     def __repr__(self):
+        if self.type == Type.Null: return "Null"
         return "{}".format(self.val)
 
 class BinExp:
@@ -22,13 +45,6 @@ class BinExp:
     def __repr__(self):
         return "({} {} {})".format(self.op, self.left, self.right)
 
-op_table = {
-    '+': lambda x, y: x + y,
-    '-': lambda x, y: x - y,
-    '*': lambda x, y: x * y,
-    '/': lambda x, y: x / y
-}
-
 def eval(node):
     if isinstance(node, Literal):
         return node.val
@@ -36,28 +52,44 @@ def eval(node):
         left = eval(node.left)
         right = eval(node.right)
         return op_table[node.op](left, right)
+    elif isinstance(node, Stmt):
+        if node.kind == StmtKind.Print: print(eval(node.exp))
+    elif isinstance(node, Decl):
+        if node.name in global_env: 
+            raise ValueError('Identifier {} already declared')
+        else: 
+            global_env[node.name] = eval(node.exp)
     else:
         raise TypeError('Type not recognized: {}'.format(type(node)))
 
 parse_grammar = r"""
     ws = (' ' | '\r' | '\n' | '\t')*
     digit = anything:x ?(x in '0123456789') -> x
-    num = <digit+>:ds -> int(ds) 
+    num = <digit+>:ds -> int(ds)
+    identifier = <letter+>:first <letterOrDigit*>:rest -> first + rest
 
-    primary = ws num:n ws                       -> Literal(Type.Num, n)
-            | ws '(' exp:e ')' ws               -> e
-    fac     = fac:x ('*' | '/'):op primary:y    -> BinExp(x, op, y)
-            | primary
-    exp     = exp:x ('+' | '-'):op fac:y        -> BinExp(x, op, y)
-            | fac
-    program = exp
+    primary   = ws num:n ws                             -> Literal(Type.Num, n)
+              | ws '(' exp:e ')' ws                     -> e
+    fac       = fac:x ('*' | '/'):op primary:y          -> BinExp(x, op, y)
+              | primary
+    exp       = exp:x ('+' | '-'):op fac:y              -> BinExp(x, op, y)
+              | fac
+    printStmt = ws 'print' exp:e ';'                    -> Stmt(StmtKind.Print, e)
+    init      = '=' exp:e                               -> e
+    declStmt  = ws 'var' ws identifier:i ws init?:e ';' -> Decl(i, e)
+    stmt      = printStmt
+              | declStmt
+    program   = stmt*
 """
 
 def parse(source):
     bindings = {
         'Type':Type,
         'Literal':Literal,
-        'BinExp':BinExp
+        'StmtKind':StmtKind,
+        'Stmt':Stmt,
+        'BinExp':BinExp,
+        'Decl':Decl
     }
     parser = makeGrammar(parse_grammar, bindings)
     ast = parser(source).program()
@@ -73,12 +105,15 @@ def main():
         with open(sys.argv[1]) as f:
             source = f.read()
         ast = parse(source)
-        print(eval(ast))
+        print(ast)
+        for stmt in ast:
+            eval(stmt)              
     else:
         # REPL mode
         while True:
             source = input('>> ')
             ast = parse(source)
-            print(eval(ast))
+            for stmt in ast:
+                eval(stmt)
 
 if __name__ == "__main__": main()
--- test.sol
+++ test.sol
@@ -1,1 +1,3 @@
- 1 + 2
+print 3 * 2;
+var name;
+var name2 = 5;
